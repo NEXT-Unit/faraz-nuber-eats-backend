a902e8738ef7d593108970eb02d20dadc16b1637
Faraz <farazzkamali80@gmail.com>
2023-01-11 13:38:52 +0330

    feat: creat creatAccount func & error handeling

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
+import { CreateAccountInput } from './dtos/create-account.dto';
 import { User } from './entities/user.entity';
 
 @Injectable()
@@ -8,4 +9,20 @@ export class UsersService {
   constructor(
     @InjectRepository(User) private readonly users: Repository<User>,
   ) {}
+  async createAccount({
+    email,
+    password,
+    role,
+  }: CreateAccountInput): Promise<[boolean, string?]> {
+    try {
+      const exists = await this.users.findOneBy({ email });
+      if (exists) {
+        return [false, 'There is a user with this email already'];
+      }
+      await this.users.save(this.users.create({ email, password, role }));
+      return [true];
+    } catch (error) {
+      return [false, "Couldn't create account"];
+    }
+  }
 }
