0335136957dff6a45a35b36c98db59fae3191ecb
Faraz <farazzkamali80@gmail.com>
2023-01-15 12:54:17 +0330

    feat: create login func

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { CreateAccountInput } from './dtos/create-account.dto';
+import { LoginInput } from './dtos/login.dto';
 import { User } from './entities/user.entity';
 
 @Injectable()
@@ -23,6 +24,38 @@ export class UsersService {
       return [true];
     } catch (error) {
       return [false, "Couldn't create account"];
+    }
+  }
+
+  async login({
+    email,
+    password,
+  }: LoginInput): Promise<{ ok: boolean; error?: string; token?: string }> {
+    // find user with the email
+    try {
+      const user = await this.users.findOneBy({ email });
+      if (!User) {
+        return {
+          ok: false,
+          error: 'User does not exist',
+        };
+      }
+      const PasswordCorrect = await user.checkPassword(password);
+      if (!PasswordCorrect) {
+        return {
+          ok: false,
+          error: 'wrong password',
+        };
+      }
+      return {
+        ok: true,
+        token: 'llllllllllllllllllllllllll',
+      };
+    } catch (error) {
+      return {
+        ok: false,
+        error,
+      };
     }
   }
 }
