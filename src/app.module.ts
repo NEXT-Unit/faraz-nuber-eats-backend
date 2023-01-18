d979707df917b1d89a95d5bf13cae5e77761a754
Faraz <farazzkamali80@gmail.com>
2023-01-18 12:13:03 +0330

    ok

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,10 @@
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
-import { Module } from '@nestjs/common';
+import {
+  MiddlewareConsumer,
+  Module,
+  NestModule,
+  RequestMethod,
+} from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
 import { RestaurantsModule } from './restaurants/restaurants.module';
 import { ConfigModule } from '@nestjs/config';
@@ -11,6 +16,7 @@ import { CommonModule } from './common/common.module';
 import { User } from './users/entities/user.entity';
 import { JwtModule } from './jwt/jwt.module';
 import * as jwt from 'jsonwebtoken';
+import { jwtMiddleware } from './jwt/jwt.middleware';
 
 @Module({
   imports: [
