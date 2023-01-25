3b1a3323b3bf0d2f71334dac363726543792458e
Faraz <farazzkamali80@gmail.com>
2023-01-25 19:05:50 +0330

    feat: add Verification to entities

--- src/app.module.ts
+++ src/app.module.ts
@@ -18,6 +18,7 @@ import { JwtModule } from './jwt/jwt.module';
 import * as jwt from 'jsonwebtoken';
 import { JwtMiddleware } from './jwt/jwt.middleware';
 import { AuthModule } from './auth/auth.module';
+import { Verification } from './users/entities/verification.entity';
 
 @Module({
   imports: [
@@ -44,7 +45,7 @@ import { AuthModule } from './auth/auth.module';
       database: process.env.DB_NAME,
       synchronize: process.env.NODE_ENV !== 'prod',
       logging: true,
-      entities: [User],
+      entities: [User, Verification],
     }),
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
