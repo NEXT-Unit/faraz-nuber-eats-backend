6c56cce8c49a6fc32bc2f9cdd91485409a23f6d4
Faraz <farazzkamali80@gmail.com>
2023-04-01 13:41:25 +0430

    switch to fedora

--- test/users.e2e-spec.ts
+++ test/users.e2e-spec.ts
@@ -2,11 +2,69 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
 import { AppModule } from '../src/app.module';
-import { getConnection } from 'typeorm';
+import { getConnection, getConnectionManager } from 'typeorm';
+
+jest.mock('got', () => {
+  return {
+    post: jest.fn(),
+  };
+});
+
+const GRAPHQL_ENDPOINT = '/graphql';
 
 describe('UserModule (e2e)', () => {
   let app: INestApplication;
 
+  beforeAll(async () => {
+    const moduleFixture: TestingModule = await Test.createTestingModule({
+      imports: [AppModule],
+    }).compile();
+
+    app = moduleFixture.createNestApplication();
+    await app.init();
+
+    const connectionManager = getConnectionManager();
+    if (!connectionManager.has('default')) {
+      const options = app.get('DATABASE_CONNECTION');
+      connectionManager.create(options);
+    }
+  });
+  afterAll(async () => {
+    // Close all connections after the tests are run
+    await getConnectionManager().connections.forEach((connection) =>
+      connection.close(),
+    );
+    await app.close();
+  });
+
+  describe('createAccount', () => {
+    it('should create an account', async () => {
+      const response = await request(server)
+        .post(GRAPHQL_ENDPOINT)
+        .send({
+          query: `
+            mutation {
+              createAccount(input: {
+                email: "test@example.com",
+                password: "testpassword",
+                role: Owner
+              }) {
+                ok
+                error
+              }
+            }
+          `,
+        })
+        .expect(200);
+
+      // Investigate why the ok field is false and adjust the test accordingly
+      expect(response.body.data.createAccount.ok).toBe(true);
+      expect(response.body.data.createAccount.error).toBeNull();
+    });
+  });
+  // describe('UserModule (e2e)', () => {
+  let app: INestApplication;
+  let connections;
   beforeAll(async () => {
     const module: TestingModule = await Test.createTestingModule({
       imports: [AppModule],
@@ -14,15 +72,40 @@ describe('UserModule (e2e)', () => {
 
     app = module.createNestApplication();
     await app.init();
+    connections = connections;
   });
 
   afterAll(async () => {
-    await getConnection().dropDatabase;
+    const conn = getConnection();
+    await conn.dropDatabase();
+    await conn.close();
     app.close();
+  });
+
+  it('createAccount', () => {
+    return request(app.getHttpServer())
+      .post(GRAPHQL_ENDPOINT)
+      .send({
+        query: `
+    mutation{
+      createAccount(input:{
+        email: "newe2e21@email.com",
+        password:"newe2epassword",
+        role:Owner
+      }){
+      ok
+      error
+      }
+    }`,
+      })
+      .expect(200)
+      .expect((res) => {
+        expect(res.body.data.createAccount.ok).toBe(true);
+        expect(res.body.data.createAccount.error).toEqual(null);
+      });
   });
   it.todo('me');
   it.todo('userProfile');
-  it.todo('createAccount');
   it.todo('login');
   it.todo('editProfile');
   it.todo('verifyEmail');
