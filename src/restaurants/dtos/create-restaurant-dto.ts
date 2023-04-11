c1e138fc91be90356ef066388bed2a4509daadab
Faraz <farazzkamali80@gmail.com>
2023-04-11 11:26:36 +0430

    feat: create outPut and edit Input

--- src/restaurants/dtos/create-restaurant-dto.ts
+++ src/restaurants/dtos/create-restaurant-dto.ts
@@ -1,9 +1,22 @@
-import { InputType, OmitType } from '@nestjs/graphql';
+import {
+  Field,
+  InputType,
+  ObjectType,
+  OmitType,
+  PickType,
+} from '@nestjs/graphql';
 import { Restaurant } from '../entities/restaurants.entity';
+import { MutationOutput } from 'src/common/dtos/output.dto';
 
 @InputType()
-export class CreateRestaurantDto extends OmitType(
-  Restaurant,
-  ['id'],
-  InputType,
-) {}
+export class CreateRestaurantInput extends PickType(Restaurant, [
+  'name',
+  'coverImage',
+  'address',
+]) {
+  @Field((type) => String)
+  categoryName: string;
+}
+
+@ObjectType()
+export class CreateRestaurantOutPut extends MutationOutput {}
