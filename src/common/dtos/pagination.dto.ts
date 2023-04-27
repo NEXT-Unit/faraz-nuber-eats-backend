5c334ff8ff08cd7afaee0b5e052fa94ec51fc8aa
Faraz <farazzkamali80@gmail.com>
2023-04-27 23:57:47 +0430

    feat: create Input & Output

---
+++ src/common/dtos/pagination.dto.ts
@@ -0,0 +1,23 @@
+import {
+  Args,
+  ArgsType,
+  Field,
+  InputType,
+  Int,
+  ObjectType,
+} from '@nestjs/graphql';
+import { MutationOutput } from './output.dto';
+
+@InputType()
+export class PaginationInput {
+  @Field((type) => Int, { defaultValue: 1 })
+  page: number;
+}
+
+@ObjectType()
+export class PaginationOutput extends MutationOutput {
+  @Field((type) => Int, { nullable: true })
+  totalPages?: number;
+  @Field((type) => Int, { nullable: true })
+  totalResults?: number;
+}
