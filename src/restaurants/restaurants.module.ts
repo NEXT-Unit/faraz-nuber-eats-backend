462ed54729fe8f026584a156f052b0767cb17bca
Faraz <farazzkamali80@gmail.com>
2023-04-27 23:54:28 +0430

    feat: provide category resolver

--- src/restaurants/restaurants.module.ts
+++ src/restaurants/restaurants.module.ts
@@ -2,12 +2,13 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Restaurant } from './entities/restaurants.entity';
 import { RestaurantService } from './restaurants.service';
-import { RestaurantResolver } from './restaurants.resolver';
+import { CategoryResolver, RestaurantResolver } from './restaurants.resolver';
 import { CategoryRepository } from './repositories/category.repository';
 import { Category } from './entities/category.entity';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Restaurant, Category])],
-  providers: [RestaurantResolver, RestaurantService],
+
+  providers: [RestaurantResolver, RestaurantService, CategoryResolver],
 })
 export class RestaurantsModule {}
