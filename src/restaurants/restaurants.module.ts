7abdf403a36478a497dfc72dab0617c81e5f0896
Faraz <farazzkamali80@gmail.com>
2023-04-28 21:17:34 +0430

    feat:import Dish & provide dish resolver

--- src/restaurants/restaurants.module.ts
+++ src/restaurants/restaurants.module.ts
@@ -2,13 +2,23 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Restaurant } from './entities/restaurants.entity';
 import { RestaurantService } from './restaurants.service';
-import { CategoryResolver, RestaurantResolver } from './restaurants.resolver';
+import {
+  CategoryResolver,
+  DishResolver,
+  RestaurantResolver,
+} from './restaurants.resolver';
 import { CategoryRepository } from './repositories/category.repository';
 import { Category } from './entities/category.entity';
+import { Dish } from './entities/dish.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Restaurant, Category])],
+  imports: [TypeOrmModule.forFeature([Restaurant, Category, Dish])],
 
-  providers: [RestaurantResolver, RestaurantService, CategoryResolver],
+  providers: [
+    RestaurantResolver,
+    RestaurantService,
+    CategoryResolver,
+    DishResolver,
+  ],
 })
 export class RestaurantsModule {}
