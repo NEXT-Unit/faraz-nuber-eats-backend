a51fd633dc5612534d23c7a7862cbf6e89361ca1
farazzkamali <farazzkamali80@gmail.com>
2023-05-12 11:51:14 -0400

    feat: import common

--- src/orders/orders.module.ts
+++ src/orders/orders.module.ts
@@ -6,9 +6,14 @@ import { OrderResolver } from './orders.resolver';
 import { Restaurant } from 'src/restaurants/entities/restaurants.entity';
 import { OrderItem } from './entities/order-item.entity';
 import { Dish } from 'src/restaurants/entities/dish.entity';
+import { CommonModule } from 'src/common/common.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Order, Restaurant, OrderItem, Dish])],
+  imports: [
+    TypeOrmModule.forFeature([Order, Restaurant, OrderItem, Dish]),
+    CommonModule,
+  ],
+  
   providers: [OrderService, OrderResolver],
 })
 export class OrderModule {}
