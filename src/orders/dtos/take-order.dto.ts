8069e0056171a837b998018bad1cfcff4620cb74
farazzkamali <farazzkamali80@gmail.com>
2023-05-12 11:48:06 -0400

    feat: create input & output

---
+++ src/orders/dtos/take-order.dto.ts
@@ -0,0 +1,11 @@
+import { InputType, ObjectType, PickType } from "@nestjs/graphql";
+import { Order } from "../entities/order.entity";
+import { MutationOutput } from "src/common/dtos/output.dto";
+
+
+@InputType()
+export class TakeOrderInput extends PickType(Order,['id'] ){}
+
+
+@ObjectType()
+export class TakeOrderOutput extends MutationOutput{}
